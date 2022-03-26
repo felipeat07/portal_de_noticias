1a0c0a1bfaf5e4891f250f32b38e1cfa6900fdbb
Felipe Tavares <felipeat07@gmail.com>
2022-03-26 22:18:32 +0000

    adicionando modulo consign

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 var app = require('./config/server.js');
 
-var rotaHome = require('./app/routes/home.js');
-var rotaNoticias = require('./app/routes/noticias.js');
-var rotaFormulario = require('./app/routes/formulario_incluindo_noticia.js');
-
-rotaHome(app);
-rotaNoticias(app);
-rotaFormulario(app);
-
 app.listen(3000, function(){
     console.log('Servidor ON!');
 });
