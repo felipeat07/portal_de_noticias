9c275a39ee96e40ee1f12e03150b3164fe817e3d
Felipe Tavares <felipeat07@gmail.com>
2022-03-28 22:34:12 +0100

    colocando dbconnection via consign

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,16 +1,15 @@
-var dbConnection = require('../../config/dbConnection')
-
 const res = require('express/lib/response');
 
 module.exports = function(app){
 
-    var connection = dbConnection();
-
     app.get('/noticias', function(req,res){
+
+        var connection = app.config.dbConnection();
 
         connection.query('select * from noticias', function(error, result){
             res.render('noticias/noticias.ejs', {noticias : result});
         });
 
     });
+    
 };
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
-module.exports = function(){
+var connMYSQL = function(){
+    console.log('Conexão com BD foi estabelecida');
      return mysql.createConnection({
             host : 'localhost',
             user :  'root',
             password : '1234',
             database : 'portal_de_noticias'
         });
+};
+
+
+module.exports = function(){
+    console.log('O autoload carregou o modulo de conexão com o BD');
+    return connMYSQL;
 };
