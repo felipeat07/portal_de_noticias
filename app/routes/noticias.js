f4c05d95588b58d2161cbfb9bcd48cc5459b676e
Felipe Tavares <felipeat07@gmail.com>
2022-04-06 22:11:59 +0100

    juntando views noticia e noticias

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -12,5 +12,17 @@ module.exports = function(application){
         });
 
     });
+
+     application.get('/noticia', function(req,res){
+
+        var connection = application.config.dbConnection();
+        var noticiasModel = new application.app.models.noticiasModel;
+
+        noticiasModel.getNoticia(connection, function(error, result){
+             res.render('noticias/noticia.ejs', {noticia : result});
+        }); 
+
+    });
+
     
 };
