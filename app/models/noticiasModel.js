dff136e5346b5420ad3932fce285342e498532a7
Felipe Tavares <felipeat07@gmail.com>
2022-04-12 22:19:46 +0100

    aplicando logica back-end

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -1,17 +1,21 @@
-function Noticias(){
-
+function Noticias(connection){
+    this._connection = connection;
 }
 
 Noticias.prototype.getNoticias = function(connection, callback){
-    connection.query('select * from noticias', callback);
+    this._connection.query('select * from noticias', callback);
 }
 
 Noticias.prototype.getNoticia = function(connection, callback){
-    connection.query('select * from noticias where id_noticia = 2', callback);
+    this._connection.query('select * from noticias where id_noticia = 2', callback);
 }
 
 Noticias.prototype.salvarNoticia = function(noticia, connection, callback){
-    connection.query('insert into noticias set ?', noticia, callback)
+    this._connection.query('insert into noticias set ?', noticia, callback)
+}
+
+Noticias.prototype.get5UltimasNoticias = function(callback){
+    this._connection.query('select * from noticias order by data_criacao desc limit 5', callback)
 }
 
 
